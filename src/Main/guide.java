9d5976e3ec0a090d723c4677863469fee725106d
xuandieu2709 <xuandieu12300@gmail.com>
2023-02-28 17:49:04 +0700

    update somethingx1

--- src/Main/guide.java
+++ src/Main/guide.java
@@ -8,12 +8,12 @@ package Main;
  *
  * @author Dell
  */
-public class guide extends javax.swing.JFrame {
+public class Guide extends javax.swing.JFrame {
 
     /**
      * Creates new form guide
      */
-    public guide() {
+    public Guide() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class guide extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(guide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Guide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(guide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Guide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(guide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Guide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(guide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Guide.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new guide().setVisible(true);
+                new Guide().setVisible(true);
             }
         });
     }
